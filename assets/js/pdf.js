a929879715c46221563d2fe50f8c53de5f3cf8bf
Zupz <nkhahlemasopha@gmail.com>
2021-11-18 13:58:26 +0200

    downloading pdfs

--- assets/js/pdf.js
+++ assets/js/pdf.js
@@ -6,11 +6,28 @@ window.onload = function () {
             console.log(window);
             var opt = {
                 margin: 1,
-                filename: 'myfile.pdf',
+                filename: 'Stunting.pdf',
                 image: { type: 'jpeg', quality: 0.98 },
                 html2canvas: { scale: 2 },
                 jsPDF: { unit: 'in', format: 'letter', orientation: 'portrait' }
             };
             html2pdf().from(invoice).set(opt).save();
+
         })
+
+       document.getElementById("download2")
+        .addEventListener("click", () => {
+            const invoice2 = this.document.getElementById("invoice2");
+            console.log(invoice);
+            console.log(window);
+            var opt = {
+                margin: 1,
+                filename: 'Infant_and_Young_Child_feeding.pdf',
+                image: { type: 'jpeg', quality: 0.98 },
+                html2canvas: { scale: 2 },
+                jsPDF: { unit: 'in', format: 'letter', orientation: 'portrait' }
+            };
+            html2pdf().from(invoice2).set(opt).save();
+        })    
 }
+
