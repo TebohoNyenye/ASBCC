0b49c56d73fa6fc9f7908092dc5db8a20a1d3fcd
Tlotliso Mafantiri <michaelmafantiri@gmail.com>
2021-11-26 13:07:58 +0200

    notifications

--- assets/js/main.js
+++ assets/js/main.js
@@ -127,57 +127,16 @@ console.log(res);
 }
 
 
+
 ]);
 
 function notification(){
 
 
-    (async () => {
-        // create and show the notification
-        const showNotification = () => {
-            // create a new notification
-            const notification = new Notification('ASBCC', {
-                body: 'You have a new notification',
-                icon: './assets/images/logo.png'
-            });
-    
-            // close the notification after 10 seconds
-            setTimeout(() => {
-                notification.close();
-            }, 10 * 1000);
-    
-            // navigate to a URL when clicked
-            notification.addEventListener('click', () => {
-    
-                window.open('http://asbcc.herokuapp.com/dashboard.html', '_blank');
-            });
-        }
-    
-        // show an error message
-        const showError = () => {
-            const error = document.querySelector('.error');
-            error.style.display = 'block';
-            error.textContent = 'You blocked the notifications';
-        }
-    
-        // check notification permission
-        let granted = false;
-    
-        if (Notification.permission === 'granted') {
-            granted = true;
-        } else if (Notification.permission !== 'denied') {
-            let permission = await Notification.requestPermission();
-            granted = permission === 'granted' ? true : false;
-        }
-    
-        // show notification or error
-        granted ? showNotification() : showError();
-    
-    })();
 
 
 
-
+    
 }
 
 function getsub() {
@@ -204,7 +163,50 @@ function getsub() {
             var str="you have a new entry";
             $('#niti').show();
             $('#noti').append('<p class="text-xs">'+str+'</p>');
-            notification();
+
+
+            (async () => {
+                // create and show the notification
+                const showNotification = () => {
+                    // create a new notification
+                    const notification = new Notification('ASBCC', {
+                        body: 'You have a new notification',
+                        icon: './assets/images/logo.png'
+                    });
+            
+                    // close the notification after 10 seconds
+                    setTimeout(() => {
+                        notification.close();
+                    }, 10 * 1000);
+            
+                    // navigate to a URL when clicked
+                    notification.addEventListener('click', () => {
+            
+                        window.open('http://localhost/asbcc/dashboard.html', '_blank');
+                    });
+                }
+            
+                // show an error message
+                const showError = () => {
+                    const error = document.querySelector('.error');
+                    error.style.display = 'block';
+                    error.textContent = 'You blocked the notifications';
+                }
+            
+                // check notification permission
+                let granted = false;
+            
+                if (Notification.permission === 'granted') {
+                    granted = true;
+                } else if (Notification.permission !== 'denied') {
+                    let permission = await Notification.requestPermission();
+                    granted = permission === 'granted' ? true : false;
+                }
+            
+                // show notification or error
+                granted ? showNotification() : showError();
+            
+            })();
 
 
            }
