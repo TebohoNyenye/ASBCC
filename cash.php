a4ef68bb2797d582e96cd886e8f62cfa0265efdc
Tlotliso Mafantiri <michaelmafantiri@gmail.com>
2021-11-28 14:29:13 +0200

    go

--- cash.php
+++ cash.php
@@ -261,7 +261,7 @@
     <script src="assets/vendors/perfect-scrollbar/perfect-scrollbar.min.js"></script>
     <script src="assets/vendors/simple-datatables/simple-datatables.js"></script>
     <script src="assets/js/app.js"></script>
-   
+    <script src="assets/js/pages/dashboard.js"></script>
     <script src="assets/vendors/chartjs/Chart.min.js"></script>
     <script src="assets/vendors/apexcharts/apexcharts.min.js"></script>
     <script src="assets/js/main.js"></script>
